dad06e5851ea31e472ce880f3e751b0fb4745437
ALEX_GRAY <alexandergray37@yahoo.com>
2019-09-02 23:46:10 -0700

    finished sentences

--- madlib.py
+++ madlib.py
@@ -1,12 +1,17 @@
+#song lyrics from the Beatles called Yesterday
+
 words = {}
 
 def store_input ():
-    words ["adjective1"] = input("Enter adjective: ")
     words ["noun1"] = input("Enter noun: ")
+    words ["adjective1"] = input("Enter adjective: ")
     words ["adverb1"] = input("Enter adverb ")
     words ["verb1"] = input("Enter verb")
     words ["noun2"] = input("Enter noun")
     words ["noun3"] = input("Enter noun")
+    words ["adverb2"] = input("Enter adverb")
+    words ["pronoun1"] = input("Enter pronoun")
+    words ["adjective2"] = input("Enter adjective")
 store_input()
 
 
@@ -16,4 +21,7 @@ def print_Madlib():
     print ("oh," + "I " + words["verb1"] + " in yesterday")
     print ("Suddenly, i'm not " + words["noun2"] + " I used to be")
     print ("There's a " + words["noun3"] + " hanging over me")
+    print ("Oh, yesterday came " + words["adverb2"])
+    print ("Why " + words["pronoun1"] + " had to go I don't know she wouldn't say")
+    print ("I said something " + words["adjective2"] + ", now I long for yesterday")
 print_Madlib()
